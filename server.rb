3c6f4f2b6db3dac08760c2e71d7f5560b4cacce2
schwarro <rob.ap.schwartz@gmail.com>
2018-08-01 16:38:01 -0400

    created other erb files and completed exercise

--- server.rb
+++ server.rb
@@ -10,4 +10,23 @@ end
 
 get '/portfolio' do
   erb :gallery
-end 
+end
+
+get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['movies', 'board games', 'books']
+  erb :about_me
+end
+
+get '/favourites' do
+  @fav_links = ['http://www.youtube.com/', 'http://www.reddit.com/', 'http://ca.ign.com/']
+  erb :favourites
+end
+
+get '/' do
+  redirect to('/home')
+end
+
+get '/gallery' do
+  redirect to('/portfolio')
+end
